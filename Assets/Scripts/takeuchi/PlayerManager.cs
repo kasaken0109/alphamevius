8e5b8002081e8801a381add45a5abbdb7f0c5fba
sorude26 <sorude26@gmail.com>
2021-03-14 13:59:48 +0900

    プレイヤーのステータス関係を管理するスクリプトを追加した

---
+++ Assets/Scripts/takeuchi/PlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> ステータス関係を管理する </summary>
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager Instance { get; private set; }
+    /// <summary> 体力最大値 </summary>
+    [SerializeField] int playerMaxHP = 100;
+    /// <summary> 空腹度最大値 </summary>
+    [SerializeField] int playerMaxHunger = 100;
+    /// <summary> 水分最大値 </summary>
+    [SerializeField] int playerMaxHydrate = 100;
+    /// <summary> 現在の体力 </summary>
+    public int CurrentHP { get; private set; }
+    /// <summary> 現在の空腹度 </summary>
+    public int CurrentHunger { get; private set; }
+    /// <summary> 現在の水分値 </summary>
+    public int CurrentHydrate { get; private set; }
+    
+    /// <summary>
+    /// プレイヤーにダメージを与える
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(int damage)
+    {
+        CurrentHP -= damage;
+        if (CurrentHP <= 0)
+        {
+            CurrentHP = 0;
+            Dead();
+        }
+    }
+    /// <summary>
+    /// 死亡時の処理
+    /// </summary>
+    void Dead()
+    {
+        Player.Instance.ActionStop();
+    }
+}

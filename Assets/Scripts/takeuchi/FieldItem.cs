86bc9e2e00d505d08a39150fd423251ec063cf02
sorude26 <sorude26@gmail.com>
2021-03-19 21:31:46 +0900

    フィールドで入手できるアイテムのスクリプトFieldItemを追加した

---
+++ Assets/Scripts/takeuchi/FieldItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldItem : MonoBehaviour
+{
+    [SerializeField] ItemBaseMain item;
+    bool getFlag = false;
+
+    public void DropItem(ItemBaseMain item,Vector3 pos)
+    {
+        this.item = item;
+        transform.position = pos;
+        getFlag = false;
+        this.gameObject.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!getFlag)
+            {
+                getFlag = true;
+                ItemManage.Instance.GetItem(item);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}

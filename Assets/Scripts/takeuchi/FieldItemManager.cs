6a42e5172d7c9c6700bcb0653ce6d5f6e432291c
sorude26 <sorude26@gmail.com>
2021-03-19 21:50:20 +0900

    フィールドのすべてのアイテムを管理するFieldItemManagerを追加した

---
+++ Assets/Scripts/takeuchi/FieldItemManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldItemManager : MonoBehaviour
+{
+    public static FieldItemManager Instance { get; private set; }
+    [SerializeField] FieldItem[] fieldItems;
+    private void Awake()
+    {
+        Instance = this;
+    }
+}

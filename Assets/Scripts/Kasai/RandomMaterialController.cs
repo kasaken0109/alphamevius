cc0222f9e271757c0bb6d5acf17822ef4d2f3788
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-26 11:50:22 +0900

    randomMaterialControllerを追加した

---
+++ Assets/Scripts/Kasai/RandomMaterialController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterialController : MonoBehaviour
+{
+    [SerializeField] ItemEnum[] m_item;
+    [SerializeField] int m_size = Random.Range(0,5);
+    [SerializeField] int m_MaxDropNum = 5;
+    [SerializeField] Transform[] m_dropPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (m_size)
+        {
+            case 0: 
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

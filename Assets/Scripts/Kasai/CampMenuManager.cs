93cc8184bf43e4dce2dc2ae3a88fd68b6b3fcfc8
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-26 17:50:54 +0900

    campMenuManagerが表示されなかった問題を修正した

---
+++ Assets/Scripts/Kasai/CampMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]ActionRange actionRange;
+    [SerializeField] GameObject m_message = null;
+    void Start()
+    {
+        m_message.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actionRange.InPlayer)
+        {
+            Debug.Log("表示");
+            m_message.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("非表示");
+            m_message.SetActive(false);
+        }
+    }
+}

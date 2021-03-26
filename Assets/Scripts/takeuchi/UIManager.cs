5b0777ed691877f4762707986176b3add2171c13
sorude26 <sorude26@gmail.com>
2021-03-26 17:16:36 +0900

    UIManagerを追加した

---
+++ Assets/Scripts/takeuchi/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject ItemInventory;
+    [SerializeField] GameObject CraftInventory;
+    [SerializeField] GameObject RecycleInventory;
+    [SerializeField] GameObject CookingInventory;
+    void Start()
+    {
+        ItemInventory.SetActive(false);
+        CraftInventory.SetActive(false);
+        RecycleInventory.SetActive(false);
+        CookingInventory.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
+        {
+            ItemInventory.SetActive(true);
+            CraftInventory.SetActive(false);
+            RecycleInventory.SetActive(false);
+            CookingInventory.SetActive(false);
+        }
+        if (Input.GetButtonDown("Craft"))
+        {
+            ItemInventory.SetActive(false);
+            CraftInventory.SetActive(true);
+            RecycleInventory.SetActive(false);
+            CookingInventory.SetActive(false);
+        }
+        if (Input.GetButtonDown("Recycle"))
+        {
+            ItemInventory.SetActive(false);
+            CraftInventory.SetActive(false);
+            RecycleInventory.SetActive(true);
+            CookingInventory.SetActive(false);
+        }
+    }
+
+    public void OnClickCooking()
+    {
+        ItemInventory.SetActive(false);
+        CraftInventory.SetActive(false);
+        RecycleInventory.SetActive(false);
+        CookingInventory.SetActive(true);
+    }
+}

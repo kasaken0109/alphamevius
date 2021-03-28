1c43b95a33d51922e84f1604d6876cdaec1aa204
sorude26 <sorude26@gmail.com>
2021-03-28 23:06:12 +0900

    UIManagerにHPバー関係の設定を追加した

--- Assets/Scripts/takeuchi/UIManager.cs
+++ Assets/Scripts/takeuchi/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,9 +9,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject CraftInventory;
     [SerializeField] GameObject RecycleInventory;
     [SerializeField] GameObject CookingInventory;
+    [SerializeField] Image HPGauge;
+    [SerializeField] Image WGauge;
+    [SerializeField] Image HGauge;
     private bool open;
+    private int playerMaxHP;
+    private float R = 0;
+    private float G = 255;
+    private int B = 0;
+
     void Start()
     {
+        playerMaxHP = PlayerManager.Instance.GetMaxHP();
         ItemInventory.SetActive(false);
         CraftInventory.SetActive(false);
         RecycleInventory.SetActive(false);
@@ -19,10 +29,15 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        int p = PlayerManager.Instance.CurrentHP;
+        float pm = playerMaxHP;
+        HPGauge.fillAmount = p / pm;
+        HPGauge.color = new Color(R, G, B);
         if (Input.GetButtonDown("Menu"))
         {
             if (!open)
             {
+                PlayerManager.Instance.Damage(10);
                 ItemInventory.SetActive(true);
                 CraftInventory.SetActive(false);
                 RecycleInventory.SetActive(false);

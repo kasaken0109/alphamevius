418f281ae293cc9b8e1c10888d2f7050efcc7824
sorude26 <sorude26@gmail.com>
2021-04-27 00:43:05 +0900

    Arrowスクリプトを追加し、プレイヤーが弓を打てるように仮設定した

---
+++ Assets/Scripts/takeuchi/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] float arrowSpeed = 5f;
+    [SerializeField] float flyTime = 1f;
+    float flyTimer = 0;
+    Vector2 dir = Vector2.zero;
+    [SerializeField] Rigidbody2D rB = null;
+    void Update()
+    {
+        flyTimer += Time.deltaTime;
+        if (flyTimer >= flyTime)
+        {
+            Destroy(this.gameObject);
+        }
+        rB.velocity = dir.normalized * arrowSpeed;
+    }
+    public void SetArrowDir(Vector2 dir)
+    {
+        this.dir = dir;
+        transform.rotation = Quaternion.FromToRotation(Vector2.left, dir);
+    }
+}

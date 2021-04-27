8f6e537377c9217957fa8b05e9c549148bc43cd3
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-04-27 18:51:35 +0900

    時計UIの設定を調整した

---
+++ Assets/Scripts/Kasai/Materials/FieldObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldObject : MonoBehaviour
+{
+    [SerializeField] protected ItemEnum[] DropItems;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FieldItemManager.Instance.DropMaterial(DropItems, transform.position);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Saito/newScripts/NewItem.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MaterialType
+{
+    None,
+    Cloth,
+    Plastic,
+    Aluminium,
+    Glass,
+    Wood,
+    Stone,
+    Metal,
+    Feathers,
+    Bone,
+    Fur,
+    Copper,
+    Herbs,
+    Yarn,
+    Cement,
+    Charcoal,
+    Gunpowder,
+    RagCloth,
+    Bottle,
+    vinyl,
+    Can,
+    GqFeathers,
+    GqFur,
+    GqYarn,
+    Gold,
+    Crystal,
+    Titanium,
+    MeatBird,
+    MeatDeer,
+    BigMeat,
+    MeatSpider
+}
+public enum ToolType
+{
+    None,
+    Knife,
+    Bow,
+    Axe,
+    Hammer,
+    Machete,
+    Pickaxe,
+    Clothes,
+    Trap,
+    HealingHP,
+    Torch,
+    HealingWater,
+    Bridge,
+    Cooking
+}
+
+public class NewItem : ScriptableObject
+{
+    [SerializeField] string m_name;
+    [SerializeField] Sprite m_sprite;
+    [SerializeField] MaterialType[] m_needMaterials;
+    [SerializeField] MaterialType[] m_getMaterials;
+    [SerializeField] int ID;
+    int haveNumber = 0;
+    public string GetName()
+    {
+        return m_name;
+    }
+    public Sprite GetSprite()
+    {
+        return m_sprite;
+    }
+    public MaterialType[] GetNeedMaterials()
+    {
+        return m_needMaterials;
+    }
+    public MaterialType[] GetGetMaterials()
+    {
+        return m_getMaterials;
+    }
+    public int GetID()
+    {
+        return ID;
+    }
+    public int GetHaveNumber() { return haveNumber; }
+    public void AddHaveNumber(int getNumber)
+    {
+        haveNumber += getNumber;
+    }
+    public void SubHaveNumber(int number)
+    {
+        haveNumber -= number;
+    }
+    public void Zero()
+    {
+        haveNumber = 0;
+    }
+    public virtual ToolType GetToolType()
+    {
+        return ToolType.None;
+    }
+}

b7d9cbbd14e4532a77633d4a662c3e3738cbaf1a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-04-28 17:49:12 +0900

    ミニマップにプレイヤーアイコンが表示される機能を実装した

---
+++ Assets/Scripts/Kasai/Script/MapManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_mapCamera = null;
+    bool mapActive = false;
+    //[SerializeField] 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveMap()
+    {
+        Debug.Log("MapActive");
+        if (!mapActive)
+        {
+            m_mapCamera.SetActive(true);
+            mapActive = true;
+        }
+        else
+        {
+            m_mapCamera.SetActive(false);
+            mapActive = false;
+        }
+        
+    }
+}

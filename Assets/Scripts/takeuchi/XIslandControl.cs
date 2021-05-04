c536bf4762faf5733c4439e31a6574a97508bee5
sorude26 <sorude26@gmail.com>
2021-05-04 19:57:14 +0900

    テスト用のアイテムポップスクリプトを追加した

---
+++ Assets/Scripts/takeuchi/XIslandControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XIslandControl : MonoBehaviour
+{
+    [SerializeField] Transform[] itemPos;
+    void Start()
+    {
+        for (int i = 0; i < itemPos.Length; i++)
+        {
+            XItemPopControler.Instance.SetPosition(itemPos[i]);
+        }
+    }
+}

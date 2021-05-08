60810e969c9bceb1e02ed6bd7482f3edfc3902aa
sorude26 <sorude26@gmail.com>
2021-05-08 15:35:52 +0900

    MessgaeManagerを追加した

---
+++ Assets/Scripts/takeuchi/MessgaeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessgaeManager : MonoBehaviour
+{
+    [SerializeField] Text messageText;
+    [SerializeField] float viweTime = 3f;
+    float viweTimer;
+    static MessgaeManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Update()
+    {
+        if (viweTimer <= 0)
+        {
+            return;
+        }
+        viweTimer -= Time.deltaTime;
+        if (viweTimer <= 0)
+        {
+            messageText.text = "";
+        }
+    }
+    public static void ViweMessgae(string viweText)
+    {
+        instance.messageText.text = viweText;
+        instance.viweTimer = instance.viweTime;
+    }
+}

f9b1c84523e2a6d9beb44df0b508ddbfe7ecfd22
sorude26 <sorude26@gmail.com>
2021-05-13 11:16:03 +0900

    ストーリー表示用スクリプトを追加した

---
+++ Assets/Scripts/takeuchi/StoryTextController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryTextController : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] StoryTextBox[] textBoxs;
+    string[] storyTextList;
+    bool viweStory = false;
+    void Update()
+    {
+        if (!viweStory)
+        {
+            return;
+        }
+    }
+    public void SetStory(int storyNumber)
+    {
+        if (storyNumber >= textBoxs.Length)
+        {
+            return;
+        }
+        storyTextList = textBoxs[storyNumber].GetStory();
+    }
+}

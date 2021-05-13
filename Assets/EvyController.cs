3e64824df5a6e6a8c6645da9121329a11a9d8191
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-13 18:12:27 +0900

    Logの表記を修正した

--- Assets/EvyController.cs
+++ Assets/EvyController.cs
@@ -3,41 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class EvyController : MonoBehaviour
 {
     [SerializeField] TimeManager timeManager = null;
     [SerializeField] bool IsNightEvy = true;
     [SerializeField] float m_moveDistance = 3;
     [SerializeField] float m_moveSpeed = 0.5f;
-    [SerializeField] bool IsMoveUp = true;
     [SerializeField] Transform m_target;
-    float m_moveDirection = 1;
-    Animation m_anim;
-    Rigidbody2D m_rb;
     Vector3 m_origin;
     // Start is called before the first frame update
     void Start()
     {
         timeManager = GameObject.Find("GameManager").GetComponent<TimeManager>();
-        m_rb = GetComponent<Rigidbody2D>();
-        m_origin = transform.position;
+        m_origin = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //timeManager.dayStatus = TimeManager.Instance.GetDayStatus();
+        timeManager.dayStatus = TimeManager.Instance.GetDayStatus();
         Debug.Log(timeManager.dayStatus);
-        Debug.Log(Vector2.Distance(transform.position, m_origin));
+        Debug.Log(Vector2.Distance(transform.localPosition, m_origin));
         if (IsNightEvy)
         {
             if (timeManager.dayStatus == TimeManager.DayStatus.NIGHT)
             {
+                Debug.Log("NightActive");
                 Move();
             }
             else
             {
+                Debug.Log("NightNonActive");
                 MoveBack();
             }
         }
@@ -45,6 +41,7 @@ public class EvyController : MonoBehaviour
         {
             if (timeManager.dayStatus == TimeManager.DayStatus.NOON)
             {
+                Debug.Log("NoonActive");
                 Move();
             }
             else
@@ -62,10 +59,5 @@ public class EvyController : MonoBehaviour
     private void MoveBack()
     {
         this.transform.DOMove(m_origin, 30f);
-    }
-
-    private void Stop()
-    {
-        m_rb.velocity = new Vector2(0, 0);
     }
 }
---
+++ Assets/Scripts/Kasai/Script/LogManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogManager : MonoBehaviour
+{
+    public static  LogManager Instance { get; private set; }
+    List <LogController>logList;
+    [SerializeField] GameObject log = null;
+    private int m_maxNum = 3;
+    int index = 0;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        logList = new List<LogController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowLog(string text, int num)
+    {
+        GameObject m_logs = Instantiate(log);
+        m_logs.transform.SetParent(this.gameObject.transform);
+        LogController logs = m_logs.GetComponent<LogController>();
+        logList.Add(logs);
+        index++;
+        if (index > m_maxNum)
+        {
+            logList.Remove(logList[0]);
+            Destroy(logList[0]);
+            index = 1;
+        }
+        logList[index - 1].DisplayLog(text + " * " + num);
+    }
+}

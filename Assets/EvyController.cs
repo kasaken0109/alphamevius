99e41af752c186b087895692db0cce191e673134
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-14 11:03:55 +0900

    EvyControllerを新仕様に修正した

--- Assets/EvyController.cs
+++ Assets/EvyController.cs
@@ -5,8 +5,6 @@ using DG.Tweening;
 
 public class EvyController : MonoBehaviour
 {
-    [SerializeField] TimeManager timeManager = null;
-    [SerializeField] bool IsNightEvy = true;
     [SerializeField] float m_moveDistance = 3;
     [SerializeField] float m_moveSpeed = 0.5f;
     [SerializeField] Transform m_target;
@@ -14,41 +12,12 @@ public class EvyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeManager = GameObject.Find("GameManager").GetComponent<TimeManager>();
         m_origin = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeManager.dayStatus = TimeManager.Instance.GetDayStatus();
-        Debug.Log(timeManager.dayStatus);
-        Debug.Log(Vector2.Distance(transform.localPosition, m_origin));
-        if (IsNightEvy)
-        {
-            if (timeManager.dayStatus == TimeManager.DayStatus.NIGHT)
-            {
-                Debug.Log("NightActive");
-                Move();
-            }
-            else
-            {
-                Debug.Log("NightNonActive");
-                MoveBack();
-            }
-        }
-        else
-        {
-            if (timeManager.dayStatus == TimeManager.DayStatus.NOON)
-            {
-                Debug.Log("NoonActive");
-                Move();
-            }
-            else
-            {
-                MoveBack();
-            }
-        }
     }
 
     private void Move()

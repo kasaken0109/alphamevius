915f6cd9c47e7400bf8bab230e47b2f299c92039
sorude26 <sorude26@gmail.com>
2021-05-21 18:40:23 +0900

    ストーリー関係のスクリプトを調整した

--- Assets/Scripts/takeuchi/StoryTextBox.cs
+++ Assets/Scripts/takeuchi/StoryTextBox.cs
@@ -6,8 +6,18 @@ using UnityEngine;
 public class StoryTextBox : ScriptableObject
 {
     [SerializeField] string[] storyText;
+    [SerializeField] Sprite[] imageList;
+    [SerializeField] int[] imageCount;
     public string[] GetStory()
     {
         return storyText;
+    }
+    public Sprite GetSprite(int imageNumber)
+    {
+        return imageList[imageNumber];
+    }
+    public int[] GetImageCount()
+    {
+        return imageCount;
     }
 }

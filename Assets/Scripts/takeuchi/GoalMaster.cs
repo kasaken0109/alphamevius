d4fdca8839588102e19fde6b94f6359f0879c22e
sorude26 <sorude26@gmail.com>
2021-05-22 01:32:36 +0900

    遺跡を仮作成した

---
+++ Assets/Scripts/takeuchi/GoalMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalMaster : MonoBehaviour
+{
+    [SerializeField] GameObject shadowObject;
+    [SerializeField] int missionItemNumber = 50;
+    bool goalF;
+    public void GoalCheck()
+    {
+        if (NewItemManager.Instance.GetItem(1).GetHaveNumber() < missionItemNumber)
+        {
+            shadowObject.SetActive(true);
+            goalF = false;
+        }
+        else
+        {
+            shadowObject.SetActive(false);
+            goalF = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (goalF)
+        {
+            if (collision.tag == "Player")
+            {
+                Debug.Log("ステージクリア!");
+                //ステージクリア処理
+            }
+        }
+    }
+}

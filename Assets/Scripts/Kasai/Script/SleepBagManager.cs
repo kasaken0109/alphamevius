8d3cc00b7478a9e4a4c7004436f0c9904ca6162a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-22 10:30:19 +0900

    SleepBagControllerを作成した

--- Assets/Scripts/Kasai/Script/SleepBagManager.cs
+++ Assets/Scripts/Kasai/Script/SleepBagManager.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class SleepBagManager : MonoBehaviour
 {
     public static SleepBagManager Instance { get; private set;}
-    TimeManager time;
-    [SerializeField] int nightTime = 20;
-    [SerializeField] int morningTime = 6;
-    [SerializeField] ActionRange actionRange = null;
-    [SerializeField] GameObject m_Button = null;
+    //TimeManager time;
+    //[SerializeField] int nightTime = 20;
+    //[SerializeField] int morningTime = 6;
+    //[SerializeField] ActionRange actionRange = null;
+    //[SerializeField] GameObject m_Button = null;
     [SerializeField] GameObject m_animSleepBag = null;
     [SerializeField] GameObject m_SleepBag = null;
     [SerializeField] Transform m_player = null;
@@ -18,21 +18,21 @@ public class SleepBagManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        time = GetComponent<TimeManager>();
+        //time = GetComponent<TimeManager>();
     }
     void Start()
     {
-        m_Button.SetActive(false);
+        //m_Button.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(actionRange.ONPlayer());
-        if (actionRange.ONPlayer())
-        {
-            m_Button.SetActive(true);
-        }
+        ////Debug.Log(actionRange.ONPlayer());
+        //if (actionRange.ONPlayer())
+        //{
+        //    m_Button.SetActive(true);
+        //}
     }
     /// <summary>
     /// 寝袋を設置するときに呼ばれる関数
@@ -43,33 +43,33 @@ public class SleepBagManager : MonoBehaviour
         if (GameObject.Find("SleepBag"))
         {
             m_SleepBag.SetActive(false);
-            Instantiate(m_animSleepBag, transform.position, transform.rotation);
+            Instantiate(m_animSleepBag, m_SleepBag.transform.position, m_SleepBag.transform.rotation);
         }
-        transform.position = m_player.position;
+        m_SleepBag.transform.position = m_player.position;
         m_SleepBag.SetActive(true);
     }
     
-    public void UseSleepBag()
-    {
-        if (TimeManager.Instance.m_hour >= nightTime && TimeManager.Instance.m_hour <= 24)
-        {
-            Debug.Log("nightleap");
-            m_SleepBag.SetActive(false);
-            Instantiate(m_animSleepBag, transform.position, transform.rotation);
-            TimeManager.Instance.m_dayNum += 1;
-            TimeManager.Instance.m_hour = morningTime;
+    //public void UseSleepBag()
+    //{
+    //    if (TimeManager.Instance.m_hour >= nightTime && TimeManager.Instance.m_hour <= 24)
+    //    {
+    //        Debug.Log("nightleap");
+    //        m_SleepBag.SetActive(false);
+    //        Instantiate(m_animSleepBag, transform.position, transform.rotation);
+    //        TimeManager.Instance.m_dayNum += 1;
+    //        TimeManager.Instance.m_hour = morningTime;
 
-        }
-        else if (TimeManager.Instance.m_hour >= 0 && TimeManager.Instance.m_hour <= morningTime)
-        {
-            Debug.Log("morningleap");
-            m_SleepBag.SetActive(false);
-            Instantiate(m_animSleepBag, transform.position, transform.rotation);
-            TimeManager.Instance.m_hour = morningTime;
-        }
-        else
-        {
-            Debug.Log("寝袋は今使えません");
-        }
-    }
+    //    }
+    //    else if (TimeManager.Instance.m_hour >= 0 && TimeManager.Instance.m_hour <= morningTime)
+    //    {
+    //        Debug.Log("morningleap");
+    //        m_SleepBag.SetActive(false);
+    //        Instantiate(m_animSleepBag, transform.position, transform.rotation);
+    //        TimeManager.Instance.m_hour = morningTime;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("寝袋は今使えません");
+    //    }
+    //}
 }

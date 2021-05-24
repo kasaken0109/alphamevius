d1c36e859d4c0628972704f03e0962cbd72e6f21
sorude26 <sorude26@gmail.com>
2021-05-24 10:27:53 +0900

    マウスに追従するスクリプトを追加した

---
+++ Assets/Scripts/takeuchi/MouseCursorControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorControl : MonoBehaviour
+{
+    private Vector3 position;
+    private Vector3 screenToWorldPointPosition;
+    void Update()
+    {
+        position = Input.mousePosition;
+        position.z = 10f;
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        gameObject.transform.position = screenToWorldPointPosition;
+    }
+}

89a44372b43ca400bd112f524fbfaa23498aa6bb
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-25 18:20:07 +0900

    ステージの修正をした

--- Assets/Scripts/takeuchi/WaterPlace.cs
+++ Assets/Scripts/takeuchi/WaterPlace.cs
@@ -6,6 +6,8 @@ public class WaterPlace : MonoBehaviour
 {
     [SerializeField] GameObject m_guide = null;
     [SerializeField] ActionRange actionRange = null;
+    [SerializeField] float m_normalHintSize = 0.5f;
+    private bool IsFirstTime = true;
 
     private void Update()
     {
@@ -17,10 +19,15 @@ public class WaterPlace : MonoBehaviour
         {
             m_guide.SetActive(false);
         }
+        if (IsFirstTime == true)
+        {
+            m_guide.gameObject.transform.localScale *= m_normalHintSize;
+        }
     }
     
     public void Drink()
     {
         PlayerManager.Instance.HealingHydrate(100);
+        IsFirstTime = false;
     }
 }

c2e54cf2c3125259200fec7e51bea09720d1b37b
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-05-28 19:24:37 +0900

    夜に遺跡ステージに入ると強制的に昼になるようにNightRemainControllerを作成した

---
+++ Assets/NightRemainController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightRemainController : MonoBehaviour
+{
+    public bool IsRemain = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsRemain = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            IsRemain = true;
+            Debug.Log(IsRemain);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            IsRemain = true;
+            Debug.Log(IsRemain);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsRemain = false;
+        Debug.Log(IsRemain);
+    }
+}

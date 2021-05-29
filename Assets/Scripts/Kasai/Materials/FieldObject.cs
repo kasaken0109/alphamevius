fb153753375aad86c0ee027e149215d105c99553
sorude26 <sorude26@gmail.com>
2021-05-29 12:16:04 +0900

    採取関連の動作を調整した

--- Assets/Scripts/Kasai/Materials/FieldObject.cs
+++ Assets/Scripts/Kasai/Materials/FieldObject.cs
@@ -6,31 +6,53 @@ public class FieldObject : MonoBehaviour
 {
     [SerializeField] protected MaterialType DropItems;
     [SerializeField] private int m_dropNum = 1;
-    // Start is called before the first frame update
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    float getTimer = 0.5f;
+    bool getF;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (getF)
+        {
+            return;
+        }
         if (collision.tag == "Player")
         {
-            if (DropItems != MaterialType.None)
-            {
-                NewItemManager.Instance.AddItem(NewItemManager.Instance.GetMaterialId(DropItems), m_dropNum);
-            }
+            getF = true;
             Player.Instance.CatchItem();
-            EffectManager.PlayEffect(EffectType.Hit, transform.position);
-            this.gameObject.SetActive(false);
+            StartCoroutine(GetGarbage());
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (getF)
+        {
+            return;
+        }
+        if (collision.tag == "Player")
+        {
+            getF = true;
+            Player.Instance.CatchItem();
+            StartCoroutine(GetGarbage());
         }
     }
 
     public MaterialType GetTouchMaterial()
     {
         return DropItems;
+    }
+    private IEnumerator GetGarbage()
+    {
+        while (getTimer > 0)
+        {
+            getTimer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        if (DropItems != MaterialType.None)
+        {
+            NewItemManager.Instance.AddItem(NewItemManager.Instance.GetMaterialId(DropItems), m_dropNum);
+        }
+        EffectManager.PlayEffect(EffectType.Hit, transform.position);
+        this.gameObject.SetActive(false);
+        getTimer = 0.5f;
     }
 }
